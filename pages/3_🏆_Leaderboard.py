a364e1468aba8b7c9ebe834b37140e3f34d6f54b
Rizki Ningrat <zeinforrell69@gmail.com>
2024-06-30 22:21:50 +0700

    small changes

--- pages/3_🏆_Leaderboard.py
+++ pages/3_🏆_Leaderboard.py
@@ -24,23 +24,23 @@ for row in rows:
     })
 
 # Menampilkan data dalam bentuk tabel
-if ('is_logged_in' not in st.session_state or 'guest' not in st.session_state):
+if ('is_logged_in' not in st.session_state and 'guest' not in st.session_state):
     st.warning("Please login first!", icon="⚠️")
-
-if 'have_played' in st.session_state:
-    edited_df = st.data_editor(
-        data_histories,
-        height=300,
-        column_order=['game_date', 'player_name', 'skor'],
-        column_config={
-            "game_date": st.column_config.TextColumn("Date", disabled=True),
-            "player_name": st.column_config.TextColumn("Name", disabled=True),
-            "skor": st.column_config.NumberColumn("Scores", disabled=True)
-        },
-        num_rows="fixed",
-        use_container_width=True,
-        hide_index=True
-    )
 else:
-    st.info("Play the game at least once..", icon="ℹ️")
+    if 'have_played' in st.session_state:
+        edited_df = st.data_editor(
+            data_histories,
+            height=300,
+            column_order=['game_date', 'player_name', 'skor'],
+            column_config={
+                "game_date": st.column_config.TextColumn("Date", disabled=True),
+                "player_name": st.column_config.TextColumn("Name", disabled=True),
+                "skor": st.column_config.NumberColumn("Scores", disabled=True)
+            },
+            num_rows="fixed",
+            use_container_width=True,
+            hide_index=True
+        )
+    else:
+        st.info("Play the game at least once..", icon="ℹ️")
 
